0ec85b0d17c3ed8ecd6b90404ed49b6a82cbaba0
rmazzoli <rafael.mazzoli@hotmail.com>
2019-11-12 22:09:00 -0300

    Estilizando notificações

---
+++ src/components/Notifications/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { MdNotifications } from 'react-icons/md';
+
+import {
+  Container,
+  Badge,
+  NotificationList,
+  Notification,
+  Scroll,
+} from './styles';
+
+export default function Notifications() {
+  return (
+    <Container>
+      <Badge hasUnread>
+        <MdNotifications color="#7159c1" size={20} />
+      </Badge>
+      <NotificationList>
+        <Scroll>
+          <Notification unread>
+            <p>Você possui um novo agedamento apra amnahã</p>
+            <time>há 02 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agedamento apra amnahã</p>
+            <time>há 02 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agedamento apra amnahã</p>
+            <time>há 02 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agedamento apra amnahã</p>
+            <time>há 02 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agedamento apra amnahã</p>
+            <time>há 02 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agedamento apra amnahã</p>
+            <time>há 02 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+          <Notification>
+            <p>Você possui um novo agedamento apra amnahã</p>
+            <time>há 02 dias</time>
+            <button type="button">Marca como lida</button>
+          </Notification>
+        </Scroll>
+      </NotificationList>
+    </Container>
+  );
+}
