fbabac2e182f9946cf3ce8eb89a4a3fb50fff483
rmazzoli <rafael.mazzoli@hotmail.com>
2019-11-12 21:18:56 -0300

    Configurando Header

--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -2,8 +2,5 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   height: 100%;
-  background: #333;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  background: linear-gradient(-90deg, #7159c1, #ab59c1);
 `;
