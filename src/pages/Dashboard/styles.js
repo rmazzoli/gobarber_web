7928eef27d5172db0027b99b0bc6d074a03aebd6
rmazzoli <rafael.mazzoli@hotmail.com>
2019-11-18 21:43:36 -0300

    Navegando entre dias

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   max-width: 600px;
-  margin: 50px auto;
+  margin: auto;
+  padding-bottom: 50px;
   display: flex;
   flex-direction: column;
   header {
